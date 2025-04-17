9ad1c35e7ef9cae01b80ae3c08e612e9fe5129e1
veyselunlusoy <veyselunlusoy@gmail.com>
2025-04-17 13:08:05 +0300

    chande1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication # type: ignore
 from  _Form import FormWindow
 
 uygulama = QApplication([])
 pencere = FormWindow()
 pencere.show()
-uygulama.exec_()
+uygulama.exec_()# # type: ignore
 
 # exe ye çevirmek için konsola aşağıdaki kod yazılacak
 # pyinstaller --windowed --onefile --onedir --clean --noconsole --add-data "rehber.db:." -w main.py
